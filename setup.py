e12cb815ddca94c28bf53aa08a9a337598e0f5cf
sharangdeo <unayaelohim@gmail.com>
2024-12-16 01:46:28 +0100

    Makefile version to be published

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='skyshapython',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     description='A Python library for utilities and wrappers for frequently used functionality.',
     long_description=open('README.md').read(),
