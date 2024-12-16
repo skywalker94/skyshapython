5bab0733b73f2c893b528930949b15592f229e49
sharangdeo <unayaelohim@gmail.com>
2024-12-16 09:30:20 +0100

    update example.py

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from skyshapython import MaxHeap, MinHeap
+from skyshapython import MaxHeap, MinHeap, SortMaster
 
 vals = [1, 77, 82, 15, 22, 43, 9, 8, 321, -3, 0]
 print(f"Raw Values: {vals}")
@@ -14,3 +14,18 @@ print(f"Descending values: {max_heap.pop(50)}")
 #One step approach
 print(f"Ascending values:{min_heap.pushpop(vals)}")
 print(f"Descending values:{max_heap.pushpop(vals)}")
+
+sm = SortMaster()
+print(f"Bubble Sorted: {sm.bubble_sort([1,2,7,4,3])}")
+print(f"Timsort Sorted: {sm.timsort([1,2,7,4,3])}")
+print(f"Quick Sorted: {sm.quick_sort([1,2,7,4,3])}")
+print(f"Thread Sorted: {sm.thread_sort([1,2,7,4,3])}")
+print(f"Fastest Algorithm with threading: {sm.fastest([1,2,7,4,3], display=True)}")
+
+print(f"Bubble Sorted: {sm.bubble_sort([1,2,7,4,3], reverse=True)}")
+print(f"Timsort Sorted: {sm.timsort([1,2,7,4,3], reverse=True)}")
+print(f"Quick Sorted: {sm.quick_sort([1,2,7,4,3], reverse=True)}")
+print(f"Thread Sorted: {sm.thread_sort([1,2,7,4,3], reverse=True)}")
+
+# One of 4 potential outputs: QuickSort, MergeSort, Timsort, IntroSort
+print(f"Fastest Algorithm with threading: {sm.fastest(list(range(100000)))}")
