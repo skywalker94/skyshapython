d0703674e84d2989be2f93d81db93c061bb0a8f3
sharangdeo <unayaelohim@gmail.com>
2024-12-16 02:32:20 +0100

    minor update t o version since functionality changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='skyshapython',
-    version='0.1.4',
+    version='0.2.0',
     packages=find_packages(),
     description='A Python library for utilities and wrappers for frequently used functionality.',
     long_description=open('README.md').read(),
